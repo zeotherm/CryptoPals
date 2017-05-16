d89815ad8d6021503ce51897c34b3340a6e256bc
Matt Ford <zeotherm@gmail.com>
2017-05-15 21:53:36 -0400

    Passed First Test

--- CryptoPals/Program.cs
+++ CryptoPals/Program.cs
@@ -10,6 +10,13 @@ namespace CryptoPals
 	{
 		static void Main(string[] args)
 		{
+			string test = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+			var b = new BasicByteMethods(test);
+
+			b.PrintData();
+
+			Console.WriteLine(b.ToBase64());
 		}
 	}
 }
---
+++ Set1Tests/Set1Challenge1Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoPals;
+
+namespace Set1Tests {
+	[TestClass]
+	public class Set1Challenge1Test {
+		private BasicByteMethods b;
+		private string input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+		private string output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+		[TestMethod]
+		public void VerifyChallenge1() {
+			b = new BasicByteMethods(input);
+			Assert.AreEqual(output, b.ToBase64());
+		}
+	}
+}
