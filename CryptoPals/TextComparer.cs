a4227dc7d60f1a5b650f6af998b66d11d1c78129
Matt Ford <zeotherm@gmail.com>
2017-05-21 19:00:47 -0400

    Finished the edit test class/tests.  Then realized that wasn't what was being asked for...

--- CryptoPals/TextComparer.cs
+++ CryptoPals/TextComparer.cs
@@ -18,17 +18,42 @@ namespace CryptoPals {
 			if( _s1.Length != _s2.Length) {
 				throw new InvalidOperationException("Hamming distance can only be computed for equal length strings");
 			}
+			//var t = _s1.Zip(_s2, (s1, s2) => new Tuple<char, char>(s1, s2));
+			//var u = t.Select(tu => tu.Item1 - tu.Item2 == 0 ? 0 : 1);
+			//var v = u.Sum();
+			//return v;
 			return _s1.Zip(_s2, (l, r) => l - r == 0 ? 0 : 1).Sum();
 		}
 
 		public int LevenshteinDistance() {
-			throw new NotImplementedException();
-
 			// https://en.wikipedia.org/wiki/Levenshtein_distance
 			// https://en.wikipedia.org/wiki/Edit_distance
 			// https://en.wikipedia.org/wiki/Hirschberg%27s_algorithm
 			// http://www.geeksforgeeks.org/dynamic-programming-set-5-edit-distance/
 			// http://users.monash.edu/~lloyd/tildeAlgDS/Dynamic/Hirsch/
+
+			int i_max = _s1.Length;
+			int j_max = _s2.Length;
+
+			int[,] m = new int[i_max+1,j_max+1];
+			m[0,0] = 0;
+			for (int i = 1; i <= i_max; i++) {
+				m[i, 0] = i;
+			}
+			for( int j = 1; j <= j_max; j++) {
+				m[0, j] = j;
+			}
+
+			for (int i = 1; i <= i_max; i++) {
+				for (int j = 1; j <= j_max; j++) {
+					int u = m[i - 1, j - 1] + (_s1[i - 1] == _s2[j - 1] ? 0 : 1);
+					int v = m[i - 1, j] + 1;
+					int w = m[i, j - 1] + 1;
+					m[i, j] = Math.Min(u, Math.Min(v, w));
+				}
+			}
+
+			return m[i_max, j_max];
 		}
 	}
 }
---
+++ Set1Tests/EditDistanceTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptoPals;
+
+namespace Set1Tests {
+	[TestClass]
+
+	public class EditDistanceTest {
+		[TestMethod]
+		public void HammingTestIdentical() {
+			string s1 = "ABCDEFG";
+			string s2 = "ABCDEFG";
+			var tc = new TextComparer(s1, s2);
+			Assert.AreEqual(0, tc.HammingDistance());
+		}
+
+		[TestMethod]
+		public void LevenshteinTestIdentical() {
+			string s1 = "ABCDEFG";
+			string s2 = "ABCDEFG";
+			var tc = new TextComparer(s1, s2);
+			Assert.AreEqual(0, tc.LevenshteinDistance());
+		}
+
+		[TestMethod]
+		public void LevenshteinTestIdenticalSimple() {
+			string s1 = "A";
+			string s2 = "A";
+			var tc = new TextComparer(s1, s2);
+			Assert.AreEqual(0, tc.LevenshteinDistance());
+		}
+
+		[TestMethod]
+		public void HammingTestDifferent() {
+			string s1 = "ABCDEFF";
+			string s2 = "ABCDEFG";
+			var tc = new TextComparer(s1, s2);
+			Assert.AreEqual(1, tc.HammingDistance());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void HammingThrow() {
+			string s1 = "ABCDEF";
+			string s2 = "ABCDEFG";
+			var tc = new TextComparer(s1, s2);
+			tc.HammingDistance();
+		}
+
+		[TestMethod]
+		public void EditLengthTest1() {
+			string s1 = "appropriate meaning";
+			string s2 = "approximate matching";
+			var tc = new TextComparer(s1, s2);
+			Assert.AreEqual(7, tc.LevenshteinDistance());
+		}
+
+		[TestMethod]
+		public void EditLengthTest2() {
+			string s1 = "this is a test";
+			string s2 = "wokka wokka!!!";
+			var tc = new TextComparer(s1, s2);
+			Assert.AreEqual(14, tc.LevenshteinDistance());
+		}
+	}
+}
