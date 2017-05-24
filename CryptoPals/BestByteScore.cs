3e29233719549355b32a985290164f81f54bcbb8
Matt Ford <zeotherm@gmail.com>
2017-05-23 21:08:45 -0400

    Solved Challenge 4 Set 1

---
+++ CryptoPals/BestByteScore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoPals
+{
+	public class BestByteScore
+	{
+		public byte BestByte { get; private set; }
+		public double Score { get; private set; }
+
+		public BestByteScore(byte b, double d)
+		{
+			BestByte = b;
+			Score = d;
+		}
+	}
+}
--- CryptoPals/Program.cs
+++ CryptoPals/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -11,27 +12,41 @@ namespace CryptoPals
 	{
 		static void Main(string[] args)
 		{
-			var e = new EnhancedByte("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
-
-			e.PrintData();
-
-			Console.WriteLine(e.ToASCII());
-
 			LanguageSample EnglishReference = new LanguageSample(Assembly.GetExecutingAssembly(), "en-corpus.txt");
 
-			Dictionary<byte, double> scores = new Dictionary<byte, double>();
-			for( byte b = 0; b < 255; b++) {
-				var ls = new LanguageSample((e ^ b).ToASCII());
-				scores.Add(b, ls.CompareTo(EnglishReference));
+			var bbs = new List<BestByteScore>();
+
+			var lines = File.ReadAllLines(@"C:\Users\Matt\Documents\Visual Studio 2015\Projects\CryptoPals\CryptoPals\S1C4.txt");
+			foreach (var line in lines)
+			{
+				var e = new EnhancedByte(line);
+				Dictionary<byte, double> scores = new Dictionary<byte, double>();
+				for (byte b = 0; b < 255; b++)
+				{
+					var ls = new LanguageSample((e ^ b).ToASCII());
+					scores.Add(b, ls.CompareTo(EnglishReference));
+				}
+				var best_byte = scores.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+				bbs.Add(new BestByteScore(best_byte, scores[best_byte]));
 			}
+			double max_score = -1.0;
+			int i_best = -1;
+			for( int i = 0; i < bbs.Count(); i++)
+			{
+				if( bbs[i].Score > max_score)
+				{
+					i_best = i;
+					max_score = bbs[i].Score;
+				}
+			}
+			Console.WriteLine(lines[i_best]);
+			var e_best = new EnhancedByte(lines[i_best]);
+			Console.WriteLine("Decoding with {0:X2} ({1}) gives \"{2}\"", bbs[i_best].BestByte, Encoding.ASCII.GetString(new[] { bbs[i_best].BestByte }), (e_best ^ bbs[i_best].BestByte).ToASCII());
 
-			var best_byte = scores.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-			Console.WriteLine("Decoding with {0:X2} ({1}) gives \"{2}\"", best_byte, Encoding.ASCII.GetString(new[] { best_byte }), (e ^ best_byte).ToASCII());
+			//var eb1 = new EnhancedByte("this is a test", bytemode.ASCII);
+			//var eb2 = new EnhancedByte("wokka wokka!!!", bytemode.ASCII);
 
-			var eb1 = new EnhancedByte("this is a test", bytemode.ASCII);
-			var eb2 = new EnhancedByte("wokka wokka!!!", bytemode.ASCII);
-
-			Console.WriteLine("Weird Hamming Distance is {0}", eb1.HammingDistance(eb2));
+			//Console.WriteLine("Weird Hamming Distance is {0}", eb1.HammingDistance(eb2));
 		}
 	}
 }
